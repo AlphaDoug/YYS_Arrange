5b2c1ed1dcd4d648af04e41c2f40900cd8d288f8
AlphaDoug <519558938@qq.com>
2019-10-16 02:28:25 +0800

    2019年10月16日02:28:18 界面

--- WindowsFormsApp1/MainForm1.Designer.cs
+++ WindowsFormsApp1/MainForm1.Designer.cs
@@ -33,10 +33,17 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm1";
+            this.Name = "MainForm1";
+            this.Text = "请选择文件或者网址";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
